3e87b7b2730bfb5f467287dbf18dfa0e51916f3a
morsy <morsyb.6c@gmail.com>
2022-02-20 22:32:14 +0200

    Log in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Customers from './components/customers/customers';
+import Login from "./Components/Login";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Customers></Customers>
+        <Login/>
       </header>
     </div>
   );
